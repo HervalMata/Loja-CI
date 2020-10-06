0fa00ff64b89763da958626018b9f76db6bf30ba
HervalMata <hervalmataamparo@gmail.com>
2020-10-05 21:25:38 -0300

    Usuários

--- application/views/restrita/layout/footer.php
+++ application/views/restrita/layout/footer.php
@@ -13,8 +13,23 @@
 <!-- Page Specific JS File -->
 <!-- Template JS File -->
 <script src="<?php echo base_url('public/assets/js/scripts.js') ?>"></script>
+
+<?php if (isset($scripts)): ?>
+	<?php foreach ($scripts as $script): ?>
+		<script src="<?php echo base_url('public/assets/' . $script); ?>"></script>
+	<?php endforeach; ?>
+<?php endif; ?>
 <!-- Custom JS File -->
 <script src="<?php echo base_url('public/assets/js/custom.js') ?>"></script>
+<script>
+    $('.delete').on("click", function (event) {
+        event.preventDefault();
+        var choice = confirm($(this).attr('data-confirm'));
+        if (choice) {
+            window.location.href = $(this).attr('href');
+        }
+    })
+</script>
 </body>
 
 
